512a4b757251d3b41d4571b6f44e5d5e96027ab6
Anshita Vishwa <holaholafires@gmail.com>
2021-08-17 16:45:39 +0530

    Added a roothandler which will tell the status of Request

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"./handlers"
 	"net/http"
 	"os"
 )
 
 func main() {
+
+	// making a handler which can serve the root
+	http.HandleFunc("/", handlers.RootHandler)
 
 	// Checking whether the port 8080 is working or not
 	err := http.ListenAndServe("localhost:8080", nil)
